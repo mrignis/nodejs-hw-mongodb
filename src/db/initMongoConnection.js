1494ccb3841dd20086179308f2269395cae15692
mrignis <chelbarah9678@gmail.com>
2024-05-21 15:25:48 -0400

    up

--- src/db/initMongoConnection.js
+++ src/db/initMongoConnection.js
@@ -6,11 +6,8 @@ dotenv.config();
 export const initMongoConnection = async () => {
   try {
     const { MONGODB_USER, MONGODB_PASSWORD, MONGODB_URL, MONGODB_DB } = process.env;
-    const mongoURI = `mongodb+srv://${MONGODB_USER}:${MONGODB_PASSWORD}@${MONGODB_URL}/${MONGODB_DB}?retryWrites=true&w=majority`;
-    await mongoose.connect(mongoURI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    const mongoURI = `mongodb://${MONGODB_USER}:${MONGODB_PASSWORD}@${MONGODB_URL}/${MONGODB_DB}`;
+    await mongoose.connect(mongoURI);
     console.log('Mongo connection successfully established!');
   } catch (error) {
     console.error('Mongo connection error:', error);
--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
-import dotenv from 'dotenv';
+import express from 'express';
 import { initMongoConnection } from './db/initMongoConnection.js';
-import { setupServer } from './server.js';
+import dotenv from 'dotenv';
 
 dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
 
 const startServer = async () => {
   try {
     await initMongoConnection();
-    setupServer();
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
   } catch (error) {
     console.error('Failed to start server:', error);
   }
