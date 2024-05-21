89911fbe62f7a16dfa6faee4d4ba4a8f08f9bd10
mrignis <chelbarah9678@gmail.com>
2024-05-21 12:55:25 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import { setupServer } from './server';
+import { setupServer } from './server.js';
 import { initMongoConnection } from './db/initMongoConnection';
 
 const startServer = async () => {
