fbe22793a81f8088cf5c70eb55dc850d656b1abd
PYLYP CHELBARAKH <143128807+mrignis@users.noreply.github.com>
2024-05-23 12:56:14 -0700

    up

--- src/services/contact.js
+++ src/services/contact.js
@@ -4,8 +4,8 @@ export const getAllContactsService = async () => {
   try {
     const contacts = await Contact.find();
     return contacts;
-  } catch (Error) {
-    throw new Error('Failed to retrieve contacts');
+  } catch (error) {
+    throw new error('Failed to retrieve contacts');
   }
 };
 
@@ -13,7 +13,7 @@ export const getContactByIdService = async (contactId) => {
   try {
     const contact = await Contact.findById(contactId);
     return contact;
-  } catch (Error) {
-    throw new Error('Failed to retrieve contact');
+  } catch (error) {
+    throw new error('Failed to retrieve contact');
   }
 };
