3b81f625e1d8e3f5d6caae53b98850b1bc249e53
PYLYP CHELBARAKH <143128807+mrignis@users.noreply.github.com>
2024-06-12 17:11:45 -0700

    Update auth.js

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,11 +1,12 @@
 import express from 'express';
 import { registerUser, loginUser, refreshSession, logoutUser } from '../controllers/auth.js';
+import { authenticate } from '../middlewares/authenticate.js';
 
 const router = express.Router();
 
 router.post('/register', registerUser);
 router.post('/login', loginUser);
-router.post('/refresh', refreshSession);
-router.post('/logout', logoutUser);
+router.post('/refresh', authenticate, refreshSession);
+router.post('/logout', authenticate, logoutUser);
 
 export default router;
