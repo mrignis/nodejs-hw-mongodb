42deee2a4472946ea13c72337bfde0f8c3bfd1f4
PYLYP CHELBARAKH <143128807+mrignis@users.noreply.github.com>
2024-06-27 14:41:07 -0700

    Update apiDocs.js

--- src/routes/apiDocs.js
+++ src/routes/apiDocs.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import swaggerUi from 'swagger-ui-express';
-import swaggerDocument from '../docs/swagger.json'  assert { type: 'json' };
+import swaggerDocument from '../docs/swagger.json' // assert { type: 'json' };
 
 
 const router = express.Router();
